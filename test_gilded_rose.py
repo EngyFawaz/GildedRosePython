3c306a0970885530fcf66678068c941006d78953
Engy Fawaz <engy.fawaz@student.guc.edu.eg>
2020-07-26 11:33:09 +0200

    code refactored and new feature added

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,10 +5,40 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", 2, 1),Item("foo", 3, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(0, items[0].quality)
+        self.assertEquals(0, items[1].quality)
+        
+
+    def test_Aged_Brie(self):
+        items = [Item("Aged Brie",20,40),Item("Aged Brie",4,50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(41, items[0].quality)
+        self.assertEquals(50, items[1].quality)
+
+
+    def test_Backstage(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert",20,40),
+        Item("Backstage passes to a TAFKAL80ETC concert",9,40),
+        Item("Backstage passes to a TAFKAL80ETC concert",4,40),
+        Item("Backstage passes to a TAFKAL80ETC concert",4,50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(41, items[0].quality)
+        self.assertEquals(42, items[1].quality)
+        self.assertEquals(43, items[2].quality)
+        self.assertEquals(50, items[3].quality) 
+
+    def test_Sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 7, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
